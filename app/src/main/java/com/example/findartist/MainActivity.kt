4cd7fa2f4dca41fd90ccdd7afd60c9d228c907a0
MirescuVictor <victormirescu2000@gmail.com>
2023-04-08 17:14:54 +0300

    added back button to app

--- app/src/main/java/com/example/findartist/MainActivity.kt
+++ app/src/main/java/com/example/findartist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.findartist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import com.example.findartist.views.LoginActivity
 
@@ -11,10 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
 
+        // Enable the ActionBar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val goToLoginButton = findViewById<Button>(R.id.goToLogin)
         goToLoginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+    }
+    // Handle the Up button when it's pressed
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finishAfterTransition()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
